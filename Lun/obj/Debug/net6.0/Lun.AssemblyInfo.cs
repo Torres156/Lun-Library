b75d1d9e933f1f4a6874222a69ef282bf9454503
Gustavo Santos <gts_br@yahoo.com.br>
2022-01-19 10:36:27 -0300

    Update package

--- Lun/obj/Debug/net6.0/Lun.AssemblyInfo.cs
+++ Lun/obj/Debug/net6.0/Lun.AssemblyInfo.cs
@@ -11,13 +11,14 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Lun")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Torres156")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lun")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lun")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/Torres156/Lun-Library")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
