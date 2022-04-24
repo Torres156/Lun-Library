31fce5fb22fe71c2b86130753bd0f1ea6b383390
Gustavo Torres <gts_br@yahoo.com.br>
2022-04-24 12:42:55 -0300

    Fix: SceneBase resize, now Size is changed before Control Resize.

--- Lun/obj/Release/netstandard2.1/Lun.AssemblyInfo.cs
+++ Lun/obj/Release/netstandard2.1/Lun.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("GitHub")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.3.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lun")]
